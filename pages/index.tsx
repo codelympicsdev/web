ef35216103d4e462d9900231cdfaa999f752a3bb
Luca Casonato <lucacasonato@yahoo.com>
2019-10-04 23:51:55 +0200

    Added basic page

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Page from '../components/page';
+
+const Home = () => (
+  <Page title='Home'>
+    <div>codelympics.dev</div>
+  </Page>
+);
+
+export default Home;
